e0a8b9daf39d5b272abf50cd6c1102c5620f12a4
n22ler <n.lerner.v3@gmail.com>
2022-11-11 15:58:15 +0200

    Create pj + Carousel

---
+++ src/components/UI/ButtonUI.vue
@@ -0,0 +1,34 @@
+<template>
+    <button class="btn" >
+        <slot></slot>
+    </button>
+</template>
+    
+<script lang="ts">
+import { defineComponent } from "vue"
+export default defineComponent({
+    name:'ButtonUI',
+    data(){
+        return{
+            isClicked:false
+        }
+                
+    },
+})
+</script>
+    
+<style scoped>
+.btn{
+    background-color: #0079C2;
+    color: white;
+    padding: 16px 32px;
+    border-radius: 50px;
+    border: none;
+    cursor: pointer;
+    font-weight: 600;
+    width: fit-content;
+    height: 52px;
+    font-size: 16px;
+    
+}
+</style>
